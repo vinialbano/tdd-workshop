7ed9d4b2d56c0ed39aeb1cb95e769a34913cc559
Marcos Schead <wssmarcos@gmail.com>
2025-06-04 21:25:28 +0000

    feat: created view messages mocked

--- apps/e2e-tests/cypress/e2e/home.cy.ts
+++
@@ -1,6 +0,0 @@
-describe("Homepage", () => {
-  it("should display the setup message", () => {
-    cy.visit("/");
-    cy.contains("This is a vanilla TypeScript + Vite + Tailwind CSS setup.");
-  });
-});
